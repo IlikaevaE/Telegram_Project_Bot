a613f130c8a68a3f72f40fc101a1a0a8adebc15d
IlikaevaE <gud_135win@mail.ru>
2022-06-29 22:42:35 +0300

    remote WD updated for Jenkins

---
+++ src/test/java/config/CredentialsConfig.java
@@ -0,0 +1,11 @@
+package config;
+
+import org.aeonbits.owner.Config;
+
+@Config.Sources("classpath:config/selenoid.properties")
+public interface CredentialsConfig extends Config{
+    String baseURL();
+    String login();
+    String password();
+    String remoteWD(); /** */
+}
